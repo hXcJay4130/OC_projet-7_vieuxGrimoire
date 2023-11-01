68403b97ea4952c2d4ccb4d22568d2abda1ca2ef
hXcJay4130 <blacklisted4life@gmail.com>
2023-11-01 15:45:02 +0100

    Installation serveur Express

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://hxc_jay:WdITi3BlxLoT7ZVx@cluster0.33sf5r7.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+app.use('/api/auth', userRoutes);
+app.use('/api/book', bookRoutes);
+ 
+module.exports = app;
+  
